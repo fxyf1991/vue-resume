ef74f62a28ba616f88a66614919f03cb7a8e6099
fxyf1991 <fxyf1991@gmail.com>
2018-03-15 20:11:31 +0800

    添加登出

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,14 @@ let app = new Vue({
             gender: '男',
             email: 'fxyf1991@gmail.com',
             phone: '18551629909'
+        },
+        signUp: {
+            email:'',
+            password:''
+        },
+        login:{
+            email:'',
+            password:''
         }
     },
     methods: {
@@ -23,13 +31,35 @@ let app = new Vue({
             } else {
                 this.saveResume()
             }
-            // let User = AV.Object.extend('User');
-            // let user = new User();
-            // user.set('resume',this.resume);
-            // user.save().then(function () {
-            // }, function (error) {
-            //     console.error(error);
-            // });
         },
+        onSignUp(e){
+            let user = new AV.User();
+            user.setUsername(this.signUp.email);
+            user.setPassword(this.signUp.password);
+            user.setEmail(this.signUp.email);
+            user.signUp().then(function (user) {
+                console.log(user);
+            }, function (error) {
+            });        },
+        onLogin(){
+            AV.User.logIn(this.login.email, this.login.password).then(function (loginedUser) {
+                console.log(loginedUser);
+            }, function (error) {
+                if (error.code === 211){
+                    alert('邮箱不存在')
+                }else if(error.code === 210){
+                    alert('邮箱和密码不匹配')
+                }
+            });
+        },
+        onLogout(){
+            AV.User.logOut();
+            alert('注销成功')
+        },
+        saveResume(){
+            let user = AV.Object.createWithoutData('User', AV.User.current().id);
+            user.set('resume', this.resume);
+            user.save();
+        }
     }
 });
