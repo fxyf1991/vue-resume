06a091033160ebae70bc05a2c17d415151fb7ed9
fxyf1991 <fxyf1991@gmail.com>
2018-03-16 17:03:35 +0800

    生成预览链接

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ let app = new Vue({
     data: {
         loginVisible: false,
         signUpVisible: false,
+        shareVisible:false,
         currentUser: {
             objectId:'',
             email:''
@@ -13,7 +14,51 @@ let app = new Vue({
             birthday: '1991年',
             gender: '男',
             email: 'fxyf1991@gmail.com',
-            phone: '18551629909'
+            phone: '18551629909',
+            skills:[
+                {
+                    name:'技能名称',
+                    description:'技能描述'
+                },
+                {
+                    name:'技能名称',
+                    description:'技能描述'
+                },
+                {
+                    name:'技能名称',
+                    description:'技能描述'
+                },
+                {
+                    name:'技能名称',
+                    description:'技能描述'
+                }
+            ],
+            projects:[
+                {
+                    name:'项目名称',
+                    link:'x.com',
+                    keywords:'关键字',
+                    description:'详细情况'
+                },
+                {
+                    name:'项目名称',
+                    link:'x.com',
+                    keywords:'关键字',
+                    description:'详细情况'
+                },
+                {
+                    name:'项目名称',
+                    link:'x.com',
+                    keywords:'关键字',
+                    description:'详细情况'
+                },
+                {
+                    name:'项目名称',
+                    link:'x.com',
+                    keywords:'关键字',
+                    description:'详细情况'
+                }
+            ]
         },
         signUp: {
             email:'',
@@ -22,11 +67,24 @@ let app = new Vue({
         login:{
             email:'',
             password:''
-        }
+        },
+        shareLink:''
     },
     methods: {
         onEdit(key, value) {
-            this.resume[key] = value
+            let regex = /\[(\d+)\]/g
+            key = key.replace(regex, (match,number)=>{
+                return `.${number}`
+            })
+            keys = key.split('.')
+            let result = this.resume
+            for(let i = 0; i < keys.length; i++){
+                if(i === keys.length-1){
+                    result[keys[i]] = value
+                }else {
+                    result = result[keys[i]]
+                }
+            }
         },
         onClickSave() {
             let currentUser = AV.User.current();
@@ -87,16 +145,29 @@ let app = new Vue({
             let query = new AV.Query('User')
             query.get(this.currentUser.objectId).then( (user) => {
                 let resume = user.toJSON().resume
-                this.resume = resume
+                Object.assign(this.resume,resume)
             }, function (error) {
                 // 异常处理
             });
-        }
+        },
+        addSkill(){
+            this.resume.skills.push({name:'技能名称',description:'技能描述'})
+        },
+        removeSkill(index){
+            this.resume.skills.splice(index,1)
+        },
+        addProject(){
+            this.resume.projects.push({name:'项目名称',link:'x.com',keywords:'关键字',description:'详细情况'})
+        },
+        removeProject(index){
+            this.resume.projects.splice(index,1)
+        },
     }
 })
 //页面加载一开始就获取用户信息
 let currentUser = AV.User.current()
 if(currentUser){
     app.currentUser = currentUser.toJSON()
+    app.shareLink = location.origin + location.pathname + '?user_id=' + app.currentUser.objectId
     app.getResume()
 }
